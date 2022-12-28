85aa8281cf05b9e28e790a19939a6596ba0bb1d4
Nirajan Karki <nirajankarki1985@gmail.com>
2022-12-28 11:21:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header setShowForm={setShowForm} />
+
+      {showForm ? <NewFactForm /> : null}
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+function Header({ showForm, setShowForm }) {
+  <header className="header">
+    <div className="logo">
+      <img src="logo.png" alt="today i learned logo" />
+      <h1>Today I Learned !</h1>
     </div>
+
+    <button
+      className="btn btn-large btn-open"
+      onClick={() => setShowForm((show) => !show)}
+    >
+      {showForm ? "Close" : "Share a fact"}
+    </button>
+  </header>;
+}
+function NewFactForm() {
+  return <form className="fact-form">new form</form>;
+}
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-category">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li className="category" key={cat.name}>
+            <button
+              className="btn btn-other-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+function FactList() {
+  const facts = initialFacts;
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => {
+          return <Fact fact={fact} key={fact.id} />;
+        })}
+      </ul>
+    </section>
+  );
+}
+function Fact({ fact }) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (source)
+        </a>
+        <span
+          className="tag"
+          style={{
+            backgroundColor: CATEGORIES.find(
+              (cat) => cat.name === fact.category
+            ).color,
+          }}
+        >
+          {fact.category}
+        </span>
+      </p>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔ {fact.votesFalse}</button>
+      </div>
+    </li>
   );
 }
 
